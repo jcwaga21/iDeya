d5a42bc34d4e24d584dca382f3f21e05d9a70460
dominiquebaes <dominiquejustine.baes@g.msuiit.edu.ph>
2019-07-04 13:55:39 +0800

    edited welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,23 @@
- @extends('layouts.app')
- @section("content")
+@extends('layouts.app')
 
+@section('content')
 <div class="container">
- <div class="row">
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <a href="{{route('co-working')}}" class="btn">Co-working</a>
-                    </div>
-                    <div class="col-md-6">
-                        <a href="" class="btn">Guest</a>
-                    </div>
-                </div>
+    <div class="row justify-content-center">
+        <div class="col-md-2">
+            Side bar
+        </div>
+        <div class="col-md-10">
+            <div class="card" style="width: 18rem;">
+                <img src="" class="card-img-top" alt="">
+            	<div class="card-header">{{$event->title}}</div>
+            	<div class="col-md-5">
+            		<a href="{{route('co-working')}}" class="btn">Co-working</a>
+            	</div>
+            	<div class="col-md-5">
+            		<a href="{{route('guest')}}" class="btn">Guest</a>
+            	</div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
