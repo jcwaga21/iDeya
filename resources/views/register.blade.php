e3d65bfe4e2b8c5ad173d9297f90ced88e827c77
dominiquebaes <dominiquejustine.baes@g.msuiit.edu.ph>
2019-07-05 13:48:11 +0800

    created registration page

---
+++ resources/views/register.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-2">
+            side bar here
+        </div>
+        <div class="col-md-10">
+            <div class="col-md-8">
+                <form method="POST" action="{{route('events.store')}}">
+                @csrf
+                <div class="form-group">
+                    <label for="first_name">First Name:</label>
+                    <input name="first_name" class="form-control" id="first_name" type="text" placeholder="Enter First Name">
+                </div>
+                <div class="form-group">
+                    <label for="last_name">Last Name:</label>
+                    <input name="last_name" class="form-control" id="last_name" type="text" placeholder="Enter Last Name">
+                </div>
+                <div class="form-group">
+                    <label for="school">School:</label>
+                    <input name="school" class="form-control" id="school" type="text" placeholder="Enter School or University">
+                </div>
+                <div class="form-group">
+                    <label for="contact">Contact:</label>
+                    <input name="contact" class="form-control" id="contact" type="text" placeholder="09XXXXXXXXX">
+                </div>
+                <div class="form-group">
+                    <label for="is_firsttime">This is my first time</label>
+                    <input name="is_firsttime" class="form-control" id="is_firsttime" type="checkbox">
+                </div>
+                <div class="form-group">
+                    <button id="register" type="submit" class="btn btn-primary">Register</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
