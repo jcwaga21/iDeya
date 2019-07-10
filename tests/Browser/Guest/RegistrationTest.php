f045cde457abb265ef3043dd9474aa5d31db1c1b
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-10 12:15:26 +0800

    Successful Guest Registration

---
+++ tests/Browser/Guest/RegistrationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Browser\Guest;
+
+use App\GuestAttendance;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegistrationTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     * @test
+     * @group guest
+     */
+    public function successfulGuestRegistration()
+    {
+        $this->browse(function (Browser $browser) {
+            $response = $browser->visit(route("home"))
+                    ->click('#guest')
+                    ->waitForText("Guest Registration")
+                    ->type('first_name','Jane')
+                    ->type('last_name','Doe')
+                    ->type('email','jane@gmail.com')
+                    ->type('contact_number','09162546254')
+                    ->type('schoolorganization','MSU-IIT')
+                    ->type('purpose','consultation')
+                    ->click('#submit');
+
+//            $guest= GuestAttendance::where(["email"=>'jane@gmail.com'])->first();
+
+            $response->assertSee('Welcome, Jane');
+        });
+    }
+}
