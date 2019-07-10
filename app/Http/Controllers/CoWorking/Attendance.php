10542c971dcf0df89131b2d72354968903f6f62a
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-10 16:31:44 +0800

    Fixed Guest Registration Request Form

--- app/Http/Controllers/CoWorking/Attendance.php
+++ app/Http/Controllers/CoWorking/Attendance.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\CoWorking;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CoWorkingRegistrationRequest;
 use App\Http\Controllers\Controller;
 use App\CoWorkingAttendance;
 
 class Attendance extends Controller
 {
-    public function __invoke(Request $request){
+    public function __invoke(CoWorkingRegistrationRequest $request){
     $user = CoWorkingAttendance::create($request->except("_token"));
     return redirect()->route("co-working.welcome", compact('user'));
 
