066838b89d36006f5739545075d33f729580bcef
dominiquebaes <dominiquejustine.baes@g.msuiit.edu.ph>
2019-07-10 17:11:05 +0800

    fixed addparticipantion bdd test

--- resources/views/event/welcome.blade.php
+++ resources/views/event/welcome.blade.php
@@ -7,7 +7,7 @@
         <div class="col-md-10">
             <div class="card text-center">
                 <img src="..." class="card-img-top" alt="...">
-                <div class="card-header">Welcome {{$user->firstname}}!</div>
+                <div class="card-header">Welcome, {{$participant->first_name}}!</div>
                 <p>With supporting text below as a natural lead-in to additional content.</p>
                 <a href="{{route('home')}}">Thanks! Back to home</a>
             </div>
