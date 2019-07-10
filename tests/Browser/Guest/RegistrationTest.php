3c83cd6eab696739ff07f9b2a9eb4f34da159f89
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-10 14:03:35 +0800

    Added validation for Handling missing filleds

--- tests/Browser/Guest/RegistrationTest.php
+++ tests/Browser/Guest/RegistrationTest.php
@@ -31,9 +31,48 @@ class RegistrationTest extends DuskTestCase
                     ->type('purpose','consultation')
                     ->click('#submit');
 
-//            $guest= GuestAttendance::where(["email"=>'jane@gmail.com'])->first();
-
             $response->assertSee('Welcome, Jane');
+        });
+    }
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     * @test
+     * @group guest
+     */
+
+    public function shouldReturnErrorIfFirstnameAndLastnameIsNotFilled()
+    {
+        $this->browse(function (Browser $browser) {
+            $response = $browser->visit(route("home"))
+                ->click('#guest')
+                ->waitForText("Guest Registration")
+                ->type('email','jane@gmail.com')
+                ->type('contact_number','09162546254')
+                ->type('schoolorganization','MSU-IIT')
+                ->type('purpose','consultation')
+                ->click('#submit');
+
+            $response->assertSee('required');
+        });
+    }
+
+    public function shouldReturnErrorIfEmailIsNotFilled()
+    {
+        $this->browse(function (Browser $browser) {
+            $response = $browser->visit(route("home"))
+                ->click('#guest')
+                ->waitForText("Guest Registration")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('contact_number','09162546254')
+                ->type('schoolorganization','MSU-IIT')
+                ->type('purpose','consultation')
+                ->click('#submit');
+
+            $response->assertSee('required');
         });
     }
 }
