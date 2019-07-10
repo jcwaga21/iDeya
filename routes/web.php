9654e0222bc6627d65c582adbfeb153806d1db53
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-10 12:12:45 +0800

    change route sequence for guest welcome route

--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,4 @@ Route::post('/co-working',"CoWorking\Attendance")->name('co-working.attendance')
 Route::get('/co-working/welcome/{user}',"CoWorking\Welcome")->name('co-working.welcome');
 
 Route::post('/guest',"Guest\Attendance")->name('guest.attendance');
-Route::get('/guest/welcome/{user}',"Guest\Welcome")->name('guest.welcome');
+Route::get('/guest/{user}/welcome',"Guest\Welcome")->name('guest.welcome');
