6ad747f0c27f797b2ce94658befbcc230e3e57e7
dominiquebaes <dominiquejustine.baes@g.msuiit.edu.ph>
2019-07-12 19:01:10 +0800

    modified the homepage

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,22 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::resource('inventory', "InventoryController");
-Route::resource('events', "EventController");
-Route::get('events/{event}/detail',"ShowDetail")->name('event.details');
-
-Route::get('events/success', function(){
-	return 'successful';
-})->name('event.success');
-
-Route::get('events/welcome', function(){
-	return 'successful';
-})->name('event.welcome');
-
+//Route::resource('events', "EventController");
 Route::get('events/{event}/register', "Event\RegistrationPageController")->name('register');
 Route::post('events/{event}/participant',"Event\AddParticipantController")
-	->name('events.participant.add');
+		->name('events.participant.add');
 Route::get('events/{participant}/welcome', "Event\WelcomeController")->name('event.welcome');
+
+Route::group(["prefix"=>'events'], function(){
+    
+    Route::get('/',"EventController@index")->name('events.index');
+    Route::get('/create', "EventController@create")->name('events.create');
+    Route::post('/', "EventController@store")->name('events.store');	
+	Route::get('/{event}/detail',"ShowDetail")->name('event.details');
+	
+});
+
+
 
 Route::view('/co-working', "co-working")->name('co-working');
 Route::view('/guest',"guest")->name('guest');
