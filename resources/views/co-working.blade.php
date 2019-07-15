469f261011bae0a15b239c25f72fc0ca7adf809e
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-15 17:28:03 +0800

    create co-working login and registration

--- resources/views/co-working.blade.php
+++ resources/views/co-working.blade.php
@@ -3,38 +3,36 @@
 @section('content')
 
 <div class="container">
-  <h1 class="">Co-Working Registration</h1><br>
-  <form method="post" action="{{route('co-working.attendance')}}">
-    @if ($errors->any())
-      <div class="alert alert-danger">
-        <ul>
-          @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-          @endforeach
-        </ul>
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+            <div class="card">
+              <div class="card-header" align="center"><h2>Change Password</h2><br>
+              <p>Please create your own password</p></div>
+              <div class="container">
+
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="#first_name">New Password</label>
+                    <input name="first_name" type="text" class="form-control" id="first_name" placeholder="e.g. Jane">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="#last_name">Last Name</label>
+                    <input name="last_name" type="text" class="form-control" id="last_name" placeholder="e.g. Doe">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="#email">Email Address</label>
+                  <input name="email" type="text" class="form-control" id="email" placeholder="username@email.com">
+                </div>
+                <div class="form-group">
+                  <label for="contact_number">Contact Number</label>
+                  <input name="contact_number" type="text" class="form-control" id="contact_number" placeholder="e.g. 0912356789">
+                </div>
+                <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    @endif
-    @csrf
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="#first_name">First Name</label>
-        <input name="first_name" type="text" class="form-control" id="first_name" placeholder="e.g. Jane">
-      </div>
-      <div class="form-group col-md-6">
-        <label for="#last_name">Last Name</label>
-        <input name="last_name" type="text" class="form-control" id="last_name" placeholder="e.g. Doe">
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="#email">Email Address</label>
-      <input name="email" type="text" class="form-control" id="email" placeholder="username@email.com">
-    </div>
-    <div class="form-group">
-      <label for="contact_number">Contact Number</label>
-      <input name="contact_number" type="text" class="form-control" id="contact_number" placeholder="e.g. 0912356789">
-    </div>
-    <button type="submit" id="submit" class="btn btn-primary">Submit</button>
-      <button type="submit" id="submit" class="btn btn-primary">Sign In</button>
-  </form>
-</div>
 @endsection
