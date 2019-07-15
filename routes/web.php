8835032048806bb3d02a29042d15490a148884cb
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-15 10:00:10 +0800

    add route for coworking log-in

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 
 Auth::routes();
 
+
+
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::resource('inventory', "InventoryController");
@@ -37,6 +39,8 @@ Route::view('/guest',"guest")->name('guest');
 
 Route::post('/co-working',"CoWorking\Attendance")->name('co-working.attendance');
 Route::get('/co-working/welcome/{user}',"CoWorking\Welcome")->name('co-working.welcome');
+Route::view('/co-working/sign-in',"co-working.sign-in")->name('co-working.sign-in');
 
 Route::post('/guest',"Guest\Attendance")->name('guest.attendance');
 Route::get('/guest/{user}/welcome',"Guest\Welcome")->name('guest.welcome');
+
