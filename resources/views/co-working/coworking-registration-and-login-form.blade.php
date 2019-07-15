500450519214d8e19f9afca058cb2627a8429eb9
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-15 17:29:21 +0800

    Rename blade from sign-in to coworking-registration-and-login-form

---
+++ resources/views/co-working/coworking-registration-and-login-form.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Login as Co-Working') }}
+                        <form class="form-inline" method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="form-group">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('email') }}</label>
+                                <div class="col-md-6">
+                                     <input type="email" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" value="{{ old('email') }}" required autofocus id="coworking-email" placeholder="Enter email" name="coworking-email">
+                                        @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                         @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password" class="col-md-4 col-form-label text-md-right"> {{ __('Password') }}</label>
+                                <div class="col-md-6">
+                                    <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" placeholder="Enter password" name="password" required>
+                                        @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                        @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="form-group row mb-0">
+                                    <div>
+                                        <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
+                                        @if (Route::has('password.request'))
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                {{ __('Forgot Your Password?') }}
+                                            </a>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+        </form><br>
+
+
+                 <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-md-14">
+                                    <div class="card">
+                                        <div class="card-header" align="center"><h2>Create New Account</h2></div>
+                                        <div class="container">
+                                            <form method="post" action="{{route('guest.attendance')}}">
+                                                @if ($errors->any())
+                                                    <div class="alert alert-danger">
+                                                        <ul>
+                                                            @foreach ($errors->all() as $error)
+                                                                <li>{{ $error }}</li>
+                                                            @endforeach
+                                                        </ul>
+                                                    </div>
+                                                @endif
+                                                @csrf
+
+                                                <div class="form-row">
+                                                    <div class="form-group col-md-6">
+                                                        <label for="#first_name">First Name</label>
+                                                        <input name="first_name" type="text" class="form-control" id="first_name" placeholder="e.g. Jane">
+                                                    </div>
+                                                    <div class="form-group col-md-6">
+                                                        <label for="#last_name">Last Name</label>
+                                                        <input name="last_name" type="text" class="form-control" id="last_name" placeholder="e.g. Doe">
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="#email">Email Address</label>
+                                                    <input name="email" type="text" class="form-control" id="email" placeholder=" e.g. username@email.com">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="contact_number">Contact Number</label>
+                                                    <input name="contact_number" type="text" class="form-control" id="contact_number" placeholder="e.g. 0912356789">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="#schoolorganization">School/Organization</label>
+                                                    <input name="schoolorganization" type="text" class="form-control" id="schoolorganization" placeholder="e.g. MSU-IIT">
+                                                </div>
+
+                                                    <div class="form-group">
+                                                        <label for="purpose">Purpose</label>
+                                                            <select name="purpose" class="form-control">
+                                                                <option value="co-working">Employee</option>
+                                                                <option value="co-working">Co-Working</option>
+                                                                <option value="internship">Internship</option>
+                                                                <option value="consultation">Consultation</option>
+                                                                <option value="visitation">Visitation</option>
+
+                                                            </select>
+                                                    </div>
+
+                                                    <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+
+                                            </form>
+                                        </div>
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+                 </div>
+
+
+
+
+
+
+
+@endsection
