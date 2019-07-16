af2d9cdcdd23d0ecb307ce3fd5de569394240033
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-16 17:28:13 +0800

    Guest Visitation Registration Testing

---
+++ tests/Browser/Registration/GuestVisitationRegistrationTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Browser\Registration;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class GuestRegistrationTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * @test
+     * @group guest
+     * @group visitation
+     */
+    public function successfulVisitationRegistration()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('email','jane@gmail.com')
+                ->type('contact_number','09358714654')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','visitation')
+                ->click('#submit')
+                ->assertSee('Welcome, Jane');
+        });
+    }
+
+    /**
+     * @test
+     * @group guest
+     * @group visitation
+     */
+    public function errorMissingEmail()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('contact_number','09358714654')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','visitation')
+                ->click('#submit')
+                ->assertSee('The email field is required.');
+        });
+    }
+
+    /**
+     * @test
+     * @group guest
+     * @group visitation
+     */
+    public function errorMissingFirstnameAndLastname()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('email','jane@gmail.com')
+                ->type('contact_number','09358714654')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','visitation')
+                ->click('#submit')
+                ->assertSee('The first name field is required.')
+                ->assertSee('The first name field is required.');
+        });
+    }
+
+    /**
+     * @test
+     * @group guest
+     * @group visitation
+     */
+    public function errorMissingContactNumber()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('email','jane@gmail.com')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','visitation')
+                ->click('#submit')
+                ->assertSee('The contact number field is required.');
+        });
+    }
+}
