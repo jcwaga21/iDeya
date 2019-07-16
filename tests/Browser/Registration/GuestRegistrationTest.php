5954654fe0d3af408ad00c8c9461bcebe01c31a9
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-16 16:58:08 +0800

    added error firstname and lastname test

--- tests/Browser/Registration/GuestRegistrationTest.php
+++ tests/Browser/Registration/GuestRegistrationTest.php
@@ -47,4 +47,24 @@ class GuestRegistrationTest extends DuskTestCase
                 ->assertSee('The email field is required.');
         });
     }
+
+    /**
+     * @test
+     * @group guest
+     */
+    public function errorMissingFirstnameAndLastname()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('email','jane@gmail.com')
+                ->type('contact_number','09358714654')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','consultation')
+                ->click('#submit')
+                ->assertSee('The first name field is required.')
+                ->assertSee('The first name field is required.');
+        });
+    }
+
 }
