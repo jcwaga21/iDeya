7c880e9335fd5782456d8e11020bfc3e66f6cb87
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-16 13:25:20 +0800

    change password page

---
+++ resources/views/co-working/changepassword.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+            <div class="card">
+              <div class="card-header" align="center"><h2>Change Password</h2><br>
+              <p>You should create a strong password that uses uppercase and lowercase letters, numbers, symbols, and spaces.</p></div>
+              <div class="container">
+
+                <div class="form-group">
+                  <label for="#email">Username</label>
+                  <input name="email" type="text" class="form-control" id="email">
+                </div>
+
+                <div class="form-group">
+                  <label for="#email">Default Password</label>
+                  <input name="email" type="text" class="form-control" id="email">
+                </div>
+
+                <div class="form-group">
+                  <label for="contact_number">New Password</label>
+                  <input name="contact_number" type="text" class="form-control" id="contact_number">
+                </div>
+
+                <div class="form-group">
+                  <label for="contact_number">Confirm New Password</label>
+                  <input name="contact_number" type="text" class="form-control" id="contact_number">
+                </div>
+
+                <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+@endsection
