6a6725fbb6d17391dc95035370ed2d3bea1c764f
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-18 16:21:42 +0800

    added negation field for employee option test

--- tests/Browser/Registration/EmployeeRegistrationTest.php
+++ tests/Browser/Registration/EmployeeRegistrationTest.php
@@ -31,4 +31,80 @@ class EmployeeRegistrationTest extends DuskTestCase
                 ->assertSee('Change Password');
         });
     }
+
+    /**
+     * @test
+     * @group employee
+     */
+    public function successfulEmployeeRegistrationWithoutSchoolOrganizationAndShouldChangePassword()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('email','jane@gmail.com')
+                ->type('contact_number','09358714654')
+                ->select('purpose','employee')
+                ->click('#submit')
+                ->assertSee('Change Password');
+        });
+    }
+
+    /**
+     * @test
+     * @group employee
+     */
+    public function errorMissingEmail()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('contact_number','09358714654')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','employee')
+                ->click('#submit')
+                ->assertSee('The email field is required.');
+        });
+    }
+
+    /**
+     * @test
+     * @group employee
+     */
+    public function errorMissingFirstnameAndLastname()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('email','jane@gmail.com')
+                ->type('contact_number','09358714654')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','employee')
+                ->click('#submit')
+                ->assertSee('The first name field is required.')
+                ->assertSee('The first name field is required.');
+        });
+    }
+
+    /**
+     * @test
+     * @group employee
+     */
+    public function errorMissingContactNumber()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('email','jane@gmail.com')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','employee')
+                ->click('#submit')
+                ->assertSee('The contact number field is required.');
+        });
+    }
 }
