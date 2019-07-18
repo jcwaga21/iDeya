adbd444007f62a8ffe737669712724ae83bdc662
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-18 16:21:09 +0800

    added negation field for coworking option test

--- tests/Browser/Registration/CoWorkingRegistrationTest.php
+++ tests/Browser/Registration/CoWorkingRegistrationTest.php
@@ -31,4 +31,80 @@ class CoWorkingRegistrationTest extends DuskTestCase
                 ->assertSee('Change Password');
         });
     }
+
+    /**
+     * @test
+     * @group coworking
+     */
+    public function successfulCoWorkingRegistrationWithoutSchoolOrganizationAndShouldChangePassword()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('email','jane@gmail.com')
+                ->type('contact_number','09358714654')
+                ->select('purpose','co-working')
+                ->click('#submit')
+                ->assertSee('Change Password');
+        });
+    }
+
+    /**
+     * @test
+     * @group coworking
+     */
+    public function errorMissingEmail()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('contact_number','09358714654')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','co-working')
+                ->click('#submit')
+                ->assertSee('The email field is required.');
+        });
+    }
+
+    /**
+     * @test
+     * @group coworking
+     */
+    public function errorMissingFirstnameAndLastname()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('email','jane@gmail.com')
+                ->type('contact_number','09358714654')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','co-working')
+                ->click('#submit')
+                ->assertSee('The first name field is required.')
+                ->assertSee('The first name field is required.');
+        });
+    }
+
+    /**
+     * @test
+     * @group coworking
+     */
+    public function errorMissingContactNumber()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->click("#Co-Working-Guest")
+                ->type('first_name','Jane')
+                ->type('last_name','Doe')
+                ->type('email','jane@gmail.com')
+                ->type('schoolorganization','MSU-IIT')
+                ->select('purpose','co-working')
+                ->click('#submit')
+                ->assertSee('The contact number field is required.');
+        });
+    }
 }
