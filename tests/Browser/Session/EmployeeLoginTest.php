90cd32f9aeb4417911b9d3cccdf70bb86cdd752b
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-07-19 16:25:18 +0800

    new Session folder

---
+++ tests/Browser/Session/EmployeeLoginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Browser\Session;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class EmployeeLoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     * @group User
+     * @test
+     */
+    public function successfullyLoggedIn()
+    {
+        factory(User::class)->create([
+            'email' => 'jcwaga21@gmail.com',
+            'password' => Hash::make('secret'),
+            'purpose' => 'employee',
+        ]);
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('co-working.coworking-registration-and-login-form'))
+                    ->type('email', 'jcwaga21@gmail.com')
+                    ->type('password', 'secret')
+                    ->click('#login')
+                    ->assertSee('Welcome!');
+        });
+    }
+}
