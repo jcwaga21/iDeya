6f08f649e872285727b6c840e1879e1beb5d8f1c
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-24 10:15:53 +0800

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ Route::group(['middleware' => 'guest'], function() {
 
     Route::post('/co-working',"CoWorking\Attendance")->name('co-working.attendance');
     Route::get('/co-working/welcome/{user}',"CoWorking\Welcome")->name('co-working.welcome');
-    Route::view('/co-working/coworking-registration-and-login-form',"co-working.coworking-registration-and-login-form")
-        ->name('co-working.coworking-registration-and-login-form');
+    Route::view('/guest/registration',"guest.registration")
+        ->name('registration');
 
     Route::post('/guest',"Guest\Attendance")->name('guest.attendance');
     Route::get('/guest/{user}/welcome',"Guest\Welcome")->name('guest.welcome');
