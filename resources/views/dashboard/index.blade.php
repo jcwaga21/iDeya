172fc942722c0f731a403b76a006805c7b3d2be2
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-07-26 16:52:30 +0800

    Added Interface in dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,38 @@
 @extends('adminlte::page')
 
 @section('content')
-<h2>Welcome!</h2>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12" style="background-color: white;">
+			<h2 class="text-center">Another Event of Ideya</h2>
+			<h3 class="text-center">August 21, 2019</h3>
+			<h4 class="text-center">Speaker: Jc Waga</h4>
+			<h4 class="text-center">Another Info: Blah Blah</h4>
+			<h4 class="text-center">Another Info: Blah Blah</h4>
+			<h4 class="text-center">Another Info: Blah Blah</h4>
+		</div>
+		<div class="row">
+			<div class="col-sm-1" style="background-color: white; margin-top: 2%;" >
+				<a href="#">
+					<h3 class="text-center">AUG</h3>
+					<h4 class="text-center">13</h4>
+				</a>
+			</div>
+			<div class="col-sm-10" style="background-color: white; margin-top: 2%; margin-left: 2%;">
+				<h2>Title</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-1" style="background-color: white; margin-top: 2%;" >
+				<a href="#">
+					<h3 class="text-center">AUG</h3>
+					<h4 class="text-center">13</h4>
+				</a>
+			</div>
+			<div class="col-sm-10" style="background-color: white; margin-top: 2%; margin-left: 2%;">
+				<h2>Title</h2>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
