64c8c68044788f384814d2e01e4b484c30f2bc37
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-07-29 13:08:18 +0800

    Added accordion for upcoming events

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,38 +1,122 @@
 @extends('adminlte::page')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-sm-12" style="background-color: white;">
-			<h2 class="text-center">Another Event of Ideya</h2>
-			<h3 class="text-center">August 21, 2019</h3>
-			<h4 class="text-center">Speaker: Jc Waga</h4>
-			<h4 class="text-center">Another Info: Blah Blah</h4>
-			<h4 class="text-center">Another Info: Blah Blah</h4>
-			<h4 class="text-center">Another Info: Blah Blah</h4>
+<div class="row container" style="background-color: white; margin-bottom: 2%;">
+	<div class="col-sm-12">
+		<h1 class="text-center">Startup Challenge 4.0</h1>
+		<h3 class="text-center">Date: August 13, 2019</h3>
+		<h3 class="text-center">Speaker: Jc Waga</h3>
+	</div>
+</div>
+
+<div class="row container" style="background-color: white;">
+	<h1 class="text-center">Upcoming Events</h1>
+	<div class="col-sm-4">
+		<div class="panel-group" id="accordion">
+		  <div class="panel panel-default">
+		    <div class="panel-heading">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+		        Startup Para Sa Mindanao: August 31, 2019</a>
+		      </h4>
+		    </div>
+		    <div id="collapse1" class="panel-collapse collapse">
+		      <div class="panel-body">
+		      	<h4>Date: August 31, 2019</h4>
+		      	<h4 class="text-danger">Speaker: None</h4>
+		      	<h4 class="text">Attendees: 20</h4>
+		      	<h4 class="text-danger">Status: Unapproved</h4>
+
+		      	<div class="pull-right"><a href="#">view</a></div>
+		      </div>
+		    </div>
+		  </div>
 		</div>
-		<div class="row">
-			<div class="col-sm-1" style="background-color: white; margin-top: 2%;" >
-				<a href="#">
-					<h3 class="text-center">AUG</h3>
-					<h4 class="text-center">13</h4>
-				</a>
-			</div>
-			<div class="col-sm-10" style="background-color: white; margin-top: 2%; margin-left: 2%;">
-				<h2>Title</h2>
-			</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="panel-group" id="accordion">
+		  <div class="panel panel-default">
+		    <div class="panel-heading">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+		        Startup Para Sa Mindanao: August 31, 2019</a>
+		      </h4>
+		    </div>
+		    <div id="collapse2" class="panel-collapse collapse">
+		      <div class="panel-body">
+		      	<h4>Date: August 31, 2019</h4>
+		      	<h4 class="text-danger">Speaker: None</h4>
+		      	<h4 class="text">Attendees: 20</h4>
+
+		      	<div class="pull-right"><a href="#">view</a></div>
+		      </div>
+		    </div>
+		  </div>
 		</div>
-		<div class="row">
-			<div class="col-sm-1" style="background-color: white; margin-top: 2%;" >
-				<a href="#">
-					<h3 class="text-center">AUG</h3>
-					<h4 class="text-center">13</h4>
-				</a>
-			</div>
-			<div class="col-sm-10" style="background-color: white; margin-top: 2%; margin-left: 2%;">
-				<h2>Title</h2>
-			</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="panel-group" id="accordion">
+		  <div class="panel panel-default">
+		    <div class="panel-heading">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+		        Startup Para Sa Mindanao</a>
+		      </h4>
+		    </div>
+		    <div id="collapse3" class="panel-collapse collapse">
+		      <div class="panel-body">
+		      	<h4>Date: August 31, 2019</h4>
+		      	<h4 class="text-danger">Speaker: None</h4>
+		      	<h4 class="text">Attendees: 20</h4>
+
+		      	<div class="pull-right"><a href="#">view</a></div>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="panel-group" id="accordion">
+		  <div class="panel panel-default">
+		    <div class="panel-heading">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+		        Startup Para Sa Mindanao</a>
+		      </h4>
+		    </div>
+		    <div id="collapse4" class="panel-collapse collapse">
+		      <div class="panel-body">
+		      	<h4>Date: August 31, 2019</h4>
+		      	<h4 class="text-danger">Speaker: None</h4>
+		      	<h4 class="text">Attendees: 20</h4>
+
+		      	<div class="pull-right"><a href="#">view</a></div>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="panel-group" id="accordion">
+		  <div class="panel panel-default">
+		    <div class="panel-heading">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+		        Startup Para Sa Mindanao</a>
+		      </h4>
+		    </div>
+		    <div id="collapse5" class="panel-collapse collapse">
+		      <div class="panel-body">
+		      	<h4>Date: August 31, 2019</h4>
+		      	<h4 class="text-danger">Speaker: None</h4>
+		      	<h4 class="text">Attendees: 20</h4>
+
+		      	<div class="pull-right"><a href="#">view</a></div>
+		      </div>
+		    </div>
+		  </div>
 		</div>
 	</div>
 </div>
+
 @endsection
