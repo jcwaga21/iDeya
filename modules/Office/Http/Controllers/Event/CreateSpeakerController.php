d04fef1d3f23b72f1cf444d651054454747608fa
dominiquebaes <dominiquejustine.baes@g.msuiit.edu.ph>
2019-08-01 10:34:49 +0800

    resolved conflict during rebase

--- modules/Office/Http/Controllers/Event/CreateSpeakerController.php
+++ modules/Office/Http/Controllers/Event/CreateSpeakerController.php
@@ -12,11 +12,6 @@ class CreateSpeakerController extends Controller
     public function __invoke(Request $request, Event $event)
     {
         $speaker = AdminEventSpeaker::all();
-
-<<<<<<< HEAD
-        return view('office::adminevent.speaker.create', compact('event'));
-=======
         return view('office::adminevent.speaker.add', compact('event'));
->>>>>>> d859e85ab98ae3ede4f53efbacfdc5404ae7c8b5
     }
 }
