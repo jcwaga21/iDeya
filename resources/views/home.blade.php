22227d5ef51eb1c063ab709513d2a2cee899a1e8
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-08-02 11:58:48 +0800

    modified UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+<!-- 
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
@@ -43,5 +43,121 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+    <div>
+        <div class="container mx-auto bg-white shadow-md mt-4">
+            <div class="block">
+                <h1 class="text-center text-5xl font-bold">Startup Challenge: Para sa Mindanao</h1>
+                <h1 class="text-center text-3xl font-semibold">Sign Up Here</h1>
+                <div class="text-center pt-2">
+                    <i class="far fa-hand-point-down text-5xl text-red-700"></i>
+                </div>
+                <div class="text-center pb-8 pt-2">
+                    <a href="{{route('registration')}}" id="Co-Working-Guest">
+                        <button class="border-red-500 border p-2 px-4 rounded-full text-red-700 bg-transparent hover:bg-red-700 hover:text-white hover:border-gray-200 font-bold">Guest</button>
+                    </a>
+                </div>
+
+                <div>
+                    <div>
+                        @unless(!$ongoingEvent)
+                        <h1>Featured</h1>
+                        <img src="">
+                        <div class="">
+                            <h5>{{$ongoingEvent->eventType->name}}</h5>
+                        </div>
+                        <div class="">
+                            <h5>Date: {{$ongoingEvent->date}}</h5>
+                        </div>
+                        <div class="">
+                            <h5>Registration Fee: {{$ongoingEvent->reg_fee}}</h5>
+                        </div>
+                        <div class="">
+                            <h5>Interested?</h5>
+                            <a href="{{route('register', compact('ongoingEvent'))}}">
+                                 <button class="border-red-500 border p-2 px-4 rounded-full text-red-700 bg-transparent hover:bg-red-700 hover:text-white hover:border-gray-200 font-bold">Register Now</button>
+                            </a>
+                        </div>
+                        <div class="">
+                            <a href="{{route('event.seedetails', compact('ongoingEvent'))}}">See Details</a>
+                        </div>
+                        @endunless
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mx-auto mt-2 w-3/5">
+            <div class="block inline-flex">
+            <div class="w-1/3 bg-white m-2 rounded-lg shadow-md">
+                <div>
+                    <div class="bg-yellow-300 h-32">
+                        
+                    </div>
+                    <div class="p-2">
+                        <p class="text-center font-bold text-xl">Grand Hackathon</p>
+                    </div>
+                    <div class="border border-gray-200"></div>
+                    <div class="p-4">
+                        <div class="block inline-flex mb-4">
+                            <i class="fas fa-calendar-alt text-2xl"></i>
+                            <p class="pl-2 -pt-1">August 13, 2019</p>
+                        </div>
+                        <div class="block inline-flex">
+                            <i class="fas fa-microphone text-2xl"></i>
+                            <p class="pl-2 -pt-1">Mrs. Dominique Baes</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-1/3 bg-white m-2 rounded-lg shadow-md">
+                <div>
+                    <div class="bg-yellow-300 h-32">
+                        
+                    </div>
+                    <div class="p-2">
+                        <p class="text-center font-bold text-xl">Grand Hackathon</p>
+                    </div>
+                    <div class="border border-gray-200"></div>
+                    <div class="p-4">
+                        <div class="block inline-flex mb-4">
+                            <i class="fas fa-calendar-alt text-2xl"></i>
+                            <p class="pl-2 -pt-1">August 13, 2019</p>
+                        </div>
+                        <div class="block inline-flex">
+                            <i class="fas fa-microphone text-2xl"></i>
+                            <p class="pl-2 -pt-1">Mrs. Dominique Baes</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-1/3 bg-white m-2 rounded-lg shadow-md">
+                <div>
+                    <div class="bg-yellow-300 h-32 shadow-md" style="background-image: url('assets/startup2.png'); " >
+                        
+                    </div>
+                    <div class="p-2">
+                        <p class="text-center font-bold text-xl text-red-800">Startup Challenge</p>
+                    </div>
+                    <div class="border border-gray-200"></div>
+                    <div class="p-4">
+                        <div class="block inline-flex mb-4 mr-4">
+                            <div><i class="fas fa-calendar-alt text-2xl text-gray-700"></i></div>
+                            <p class="pl-2 -pt-1 font-medium">August 13, 2019</p>
+                        </div>
+                        <div class="block inline-flex mb-4">
+                            <div><i class="fas fa-clock text-2xl text-gray-700"></i></div>
+                            <p class="pl-2 -pt-1 font-medium">10:00 AM - 5:00 PM</p>
+                        </div>
+                        <div class="block inline-flex">
+                            <div><i class="fas fa-microphone text-2xl text-gray-700"></i></div>
+                            <p class="pl-2 -pt-1 font-medium">Mrs. Dominique Justine B. Black</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
