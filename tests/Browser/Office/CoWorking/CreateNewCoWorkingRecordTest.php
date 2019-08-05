ff471675311faf6e6ef1af9801eb43de0339b612
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-08-05 14:01:40 +0800

    Added error missing firstname-lastname-email

--- tests/Browser/Office/CoWorking/CreateNewCoWorkingRecordTest.php
+++ tests/Browser/Office/CoWorking/CreateNewCoWorkingRecordTest.php
@@ -14,6 +14,7 @@ class CreateNewCoWorkingRecordTest extends DuskTestCase
     /**
      * A Dusk test example.
      * @test
+     * @group coworking
      * @group successfullyRecordedNewCoWorking
      * @return void
      */
@@ -38,6 +39,76 @@ class CreateNewCoWorkingRecordTest extends DuskTestCase
                 ->assertSee('Jade Doe');
 
             $this->assertDatabaseHas(
+                'users',
+                [
+                    'email' => 'jade@gmail.com',
+                    'type' => 'co-worker'
+                ]
+            );
+        });
+    }
+
+    /**
+     *
+     * @test
+     * @group coworking
+     * @group coworkingErrorMissingEmail
+     * @return void
+     */
+    public function coworkingErrorMissingEmail()
+    {
+        $this->browse(function (Browser $browser) {
+            $email = 'jc@gmail.com';
+            $this->mockAdminUser($email);
+            $response = $browser->visit(route('office.login'))
+                ->type('email', $email)
+                ->type('password', 'password')
+                ->click('#login');
+
+            $response->clickLink('Users')
+                ->click('#create-coworking')
+                ->type('first_name', 'Jade')
+                ->type('last_name', 'Doe')
+                ->type('contact_number', '09123456789')
+                ->click('#add_coworker')
+                ->assertSee('The email field is required.');
+
+            $this->assertDatabaseMissing(
+                'users',
+                [
+                    'email' => 'jade@gmail.com',
+                    'type' => 'co-worker'
+                ]
+            );
+        });
+    }
+
+    /**
+     *
+     * @test
+     * @group coworking
+     * @group coworkingErrorMissingFirstnameAndLastname
+     * @return void
+     */
+    public function coworkingErrorMissingFirstnameAndLastname()
+    {
+        $this->browse(function (Browser $browser) {
+            $email = 'jc@gmail.com';
+            $this->mockAdminUser($email);
+            $response = $browser->visit(route('office.login'))
+                ->type('email', $email)
+                ->type('password', 'password')
+                ->click('#login');
+
+            $response->clickLink('Users')
+                ->click('#create-coworking')
+                ->type('email', 'jade@gmail.com')
+                ->type('contact_number', '09123456789')
+                ->click('#add_coworker')
+                ->assertSee('The first name field is required.')
+                ->assertSee('The last name field is required.');
+
+            $this->assertDatabaseMissing(
                 'users',
                 [
                     'email' => 'jade@gmail.com',
