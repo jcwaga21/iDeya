79fc091fb07c898d06dc2f077c2055b15babc8a1
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-08-05 13:58:14 +0800

    Added AddEmployee Request in the public function

--- modules/Office/Http/Controllers/Employee/CreateEmployeeController.php
+++ modules/Office/Http/Controllers/Employee/CreateEmployeeController.php
@@ -3,14 +3,14 @@
 namespace Modules\Office\Http\Controllers\Employee;
 
 use App\User;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Hash;
 use Modules\Office\Entities\Employee;
+use Modules\Office\Http\Requests\AddEmployeeRequest;
 
 class CreateEmployeeController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(AddEmployeeRequest $request)
     {
         $data = $request->except('_token');
         Employee::create($data);
@@ -21,7 +21,6 @@ class CreateEmployeeController extends Controller
         $user['remember_token'] = $request->post('_token');
         $user['type'] = 'employee';
         User::create($user);
-
         return redirect()->route('users.listing');
     }
 }
