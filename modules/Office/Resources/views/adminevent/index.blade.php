ab68d429bb16422bba7d1ea2f6ca1e7313dc080e
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-08-05 16:23:50 +0800

    Delete dead code

--- modules/Office/Resources/views/adminevent/index.blade.php
+++ modules/Office/Resources/views/adminevent/index.blade.php
@@ -1,54 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">
-                    <lu class="list-inline">
-                        <li class="list-inline-item"><h4>Event Listings</h4></li>
-                        <a class="btn btn-primary float-right" href="{{route('adminevent.create')}}" role="button">Create Event</a>                   
-                    </lu>
-                </div>
-                <div class="card-body">
-                    <table class="table table table-bordered table-striped .table-responsive">
-                        <thead class="thead-dark">
-                        <tr>
-                            <th>Event Title</th>
-                            <th>Event Type</th>
-                            <th>Date</th>
-                            <th>Event Budget</th>
-                            <th>Number of Participants</th>
-                            <th>Guest Speaker</th>                         
-                            <th>Registration Fee</th>
-                            <th>Number of Days</th>
-                            <th>Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($events as $event)
-                                <tr>
-                                    <td> <a href="{{route('adminevent.detail', compact('event'))}}">{{$event->title}}</a> </td>
-                                    <td> {{$event->eventType->name}} </td>
-                                    <td> {{$event->date}} </td>
-                                    <td> {{optional(optional($event->eventBudget)->budget)->total_amount}} </td>
-                                    <td> {{$event->eventParticipants->count()}} / {{$event->expected_no}} </td>
-                                    <td> {{$event->guestspeaker}} </td>
-                                    <td> {{$event->reg_fee}} </td>
-                                    <td> {{$event->no_of_days}} </td>
-                                    <td> {{$event->status}} </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-
 <div class="text-gray-900 bg-gray-200">
     <div class="p-4 flex">
         <h1 class="text-3xl">
@@ -77,7 +29,7 @@
                     <td class="p-3 px-5"><a href="{{route('adminevent.detail', compact('event'))}}">{{$event->title}}</a></td>
                     <td class="p-3 px-5">{{$event->eventType->name}}</td>
                     <td class="p-3 px-5">{{$event->date}}</td>
-                    <td class="p-3 px-5">900</td>
+                    <td class="p-3 px-5"> {{optional(optional($event->eventBudget)->budget)->total_amount}}</td>
                     <td class="p-3 px-5">{{$event->eventParticipants->count()}} / {{$event->expected_no}}</td>
                     <td class="p-3 px-5">{{$event->guestspeaker}}</td>
                     <td class="p-3 px-5">{{$event->reg_fee}}</td>
