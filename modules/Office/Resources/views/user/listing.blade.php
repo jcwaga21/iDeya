4c2996d9c30c2c2df5f0beecc2962af19c745acb
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-08-06 14:42:24 +0800

    Modified UI for the user listing

--- modules/Office/Resources/views/user/listing.blade.php
+++ modules/Office/Resources/views/user/listing.blade.php
@@ -1,7 +1,7 @@
-@extends('adminlte::page')
+@extends('layouts.admin')
 
 @section('content')
-
+<!-- 
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10">
@@ -37,5 +37,45 @@
                 </div>
             </div>
         </div>
+    </div> -->
+
+    <div class="text-gray-900 bg-gray-200 container mx-auto mt-16">
+    <div class="p-4 flex">
+        <h1 class="text-3xl">
+            Event Listings
+        </h1>
+        <a href="{{route('employee.create')}}" role="button" class="bg-red-600 hover:bg-red-700 text-white text-sm px-3 py-2  border rounded-full ml-2" id="create-employee">
+            Create Employee
+        </a>
+        <a href="{{route('intern.create')}}" role="button" class="bg-yellow-400 hover:bg-yellow-500 text-white text-sm px-3 py-2  border rounded-full ml-2" id="create-intern">
+            Create Intern
+        </a>
+        <a href="{{route('coworking.create')}}" role="button" class="bg-gray-600 hover:bg-gray-700 text-white text-sm px-3 py-2  border rounded-full ml-2" id="create-coworking">
+            Create Co-worker
+        </a>
     </div>
+    <?php $i = 1;  ?>
+    <div class="px-3 py-4 justify-center">
+        <table class="w-full text-md bg-white shadow-md rounded mb-4">
+            <tbody>
+                <tr class="border-b">
+                    <th class="text-left p-3 px-5"></th>
+                    <th class="text-left p-3 px-5">Full Name</th>
+                    <th class="text-left p-3 px-5">Contact Number</th>
+                    <th class="text-left p-3 px-5">Email</th>
+                    <th class="text-left p-3 px-5">Type</th>
+                </tr>
+                @foreach($users as $user)
+                <tr class="border-b hover:bg-orange-100 bg-gray-100">
+                    <td class="p-3 px-5">{{$i++}}</td>
+                    <td class="p-3 px-5">{{$user->fullName()}}</td>
+                    <td class="p-3 px-5">{{$user->contactNumber()}}</td>
+                    <td class="p-3 px-5">{{$user->email}}</td>
+                    <td class="p-3 px-5"> {{$user->type}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
