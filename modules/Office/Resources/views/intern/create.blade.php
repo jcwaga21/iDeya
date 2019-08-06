e32cdf9942a9a0f083ce57925e85d17809bc5ccb
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-08-06 15:32:41 +0800

    Delete dead codes

--- modules/Office/Resources/views/intern/create.blade.php
+++ modules/Office/Resources/views/intern/create.blade.php
@@ -1,48 +1,6 @@
 @extends('layouts.admin')
 
 @section('content')
-
-<!--     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="col-md-8">
-                    <form method="POST" action="{{route('intern.store')}}">
-                        @csrf
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                        <h3>Create a new Intern:</h3><br>
-                        <div class="form-group">
-                            <label for="first_name">First Name:</label>
-                            <input name="first_name" class="form-control" id="first_name" type="text" placeholder="e.g. Jane">
-                        </div>
-                        <div class="form-group">
-                            <label for="last_name">Last Name:</label>
-                            <input name="last_name" class="form-control" id="last_name" type="text" placeholder="e.g. Doe">
-                        </div>
-                        <div class="form-group">
-                            <label for="contact_number">Contact Number:</label>
-                            <input name="contact_number" class="form-control" id="contact_number" type="text" placeholder="e.g. 09123456789">
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email:</label>
-                            <input name="email" class="form-control" id="email" type="text" placeholder="e.g. username@email.com">
-                        </div>
-                        <div class="form-group">
-                            <button id="add_intern" type="submit" class="btn btn-primary">Add Intern</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
     <div class="mt-20">
     <form class="w-full max-w-lg bg-white p-4 container mx-auto mt-8" method="POST" action="{{route('intern.store')}}">
                             @if ($errors->any())
