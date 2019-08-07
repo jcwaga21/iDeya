8c496e01ff45697e0ec0edfa9b73a297f5267dd2
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-08-07 16:34:36 +0800

    Change UI for welcome

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card text-center">
-                <img src="" class="card-img-top" alt="">
-                <div class="card-header"><h2>Welcome, {{$user->first_name}}!</h2></div>
-                <p>With supporting text below as a natural lead-in to additional content.</p>
-                <a href="{{route('home')}}"><h4>Thanks! Back to home</h4></a>
+<div class="container mx-auto mt-20">
+    <div class="">
+        <div class="">
+            <div class="text-center">
+                <div class=""><h2 class="text-3xl font-semibold">Welcome, {{$user->first_name}}!</h2></div>
+                <p class="leading-normal text-base md:text-2xl slide-in-bottom-subtitle">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="{{route('home')}}" class="hover:underline"><h4>Thanks! <a href="{{route('home')}}" class="hover:underline text-blue-400">Back to home</a></h4>
             </div>
         </div>
     </div>
