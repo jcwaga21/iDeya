a5cca73146b61b18cb56e2242dc7d74f3b1abfa4
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-08-08 16:30:48 +0800

    added speaker listings in module

--- modules/Office/Entities/AdminEventSpeaker.php
+++ modules/Office/Entities/AdminEventSpeaker.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class AdminEventSpeaker extends Model
 {
     protected $guarded = [];
+
+    public function fullName()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
