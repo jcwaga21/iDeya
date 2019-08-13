f46164a70514bef770585d95ecf4a287a66d247f
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-08-13 10:48:44 +0800

    Created DB for coworking Attendance

---
+++ modules/Office/Database/Migrations/2019_08_13_024008_create_co_working_attendances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoWorkingAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('co_working_attendances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('contact_number');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('co_working_attendances');
+    }
+}
