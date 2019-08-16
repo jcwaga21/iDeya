063fa551b56a7c64d1f095c446f24b90d4725663
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2019-08-16 16:19:57 +0800

    Added CDN data tables

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -29,6 +29,20 @@
     <!-- Vue.js -->
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
+        <!-- jQuery -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        
+    <!--Datatables -->
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+
+    <!--Regular Datatables CSS-->
+     <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+     <!--Responsive Extension Datatables CSS-->
+     <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
+
+
+
 
 </head>
 <body class="bg-gray-200" style="font-family: 'Source Sans Pro', sans-serif;">
